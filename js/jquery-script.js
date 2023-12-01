fe4d254e3e10d6819c4fc99934a2198a18572b6b
Gabriel Chaves <teixeira.gabriel11@gmail.com>
2023-11-30 23:09:21 -0300

    Criaçao do Index e login

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,13 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+  
+    $("#botao-cadastrar").click(function(){
+
+      $("#form-cadastrar").slideToggle("slow");
+      $("#section-login").slideToggle("slow");
+      $("#botao-cadastrar").hide();
+    
+    });
+
+});
